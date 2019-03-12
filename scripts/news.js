e1105e4b3f1d720e94eb3c2a2037ff741b826e08
mrfong <therealshadix@gmail.com>
2019-03-12 17:33:16 +0200

    changed the keyword for better results

--- scripts/news.js
+++ scripts/news.js
@@ -1,5 +1,5 @@
 
-fetch("https://newsapi.org/v2/everything?q=bitcoin&from=2019-02-12&sortBy=publishedAt&apiKey=decbac612c734abab9d1c80dc0a5f17a")
+fetch("https://newsapi.org/v2/everything?q=cryptocurrencyi&from=2019-02-12&sortBy=publishedAt&apiKey=decbac612c734abab9d1c80dc0a5f17a")
   .then((resp) => resp.json())
   .then(function (data) {
 
