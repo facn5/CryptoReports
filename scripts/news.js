5462f52928c82bf6f458b784d9b9c661c35a00d1
mrfong <therealshadix@gmail.com>
2019-03-12 12:52:25 +0200

    added the file to the news api file, linked it to the html, relates to #3

--- scripts/news.js
+++ scripts/news.js
@@ -6,4 +6,15 @@ fetch("https://newsapi.org/v2/everything?q=bitcoin&from=2019-02-12&sortBy=publis
 
     console.log(data);
 
+
   })
+
+  fetch("https://newsapi.org/v2/everything?q=bitcoin&from=2019-02-12&sortBy=publishedAt&apiKey=decbac612c734abab9d1c80dc0a5f17a
+  ")
+    .then((resp) => resp.json())
+    .then(function (data) {
+
+      console.log(data);
+
+
+    })
