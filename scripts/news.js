16ed1129524e9d84fe341c496f4e7549ee8f7bd5
mrfong <therealshadix@gmail.com>
2019-03-13 16:07:57 +0200

    fixed alot of issues, improved design, relates to #33

--- scripts/news.js
+++ scripts/news.js
@@ -41,26 +41,27 @@ fetch("https://newsapi.org/v2/everything?q=cryptocurrency&apiKey=decbac612c734ab
 
  })
  .catch(function() {
-        console.log("error");
-    });g
+
+
+    });
 
 function CheckIfImageIsNull() {
- if (document.getElementById("boximg1").src == null)
+ if (document.getElementById("boximg1").src === null)
   document.getElementById("boximg1").src = "https://image.freepik.com/free-vector/404-error-web-template-with-mad-cat_23-2147763345.jpg"
 
- if (document.getElementById("boximg2").src == null)
+ if (document.getElementById("boximg2").src === null)
   document.getElementById("boximg2").src = "https://image.freepik.com/free-vector/404-error-web-template-with-mad-cat_23-2147763345.jpg"
 
- if (document.getElementById("boximg3").src == null)
+ if (document.getElementById("boximg3").src === null)
   document.getElementById("boximg3").src = "https://image.freepik.com/free-vector/404-error-web-template-with-mad-cat_23-2147763345.jpg"
 
- if (document.getElementById("boximg4").src == null)
+ if (document.getElementById("boximg4").src === null)
   document.getElementById("boximg4").src = "https://image.freepik.com/free-vector/404-error-web-template-with-mad-cat_23-2147763345.jpg"
 
- if (document.getElementById("boximg5").src == null)
+ if (document.getElementById("boximg5").src === null)
   document.getElementById("boximg5").src = "https://image.freepik.com/free-vector/404-error-web-template-with-mad-cat_23-2147763345.jpg"
 
- if (document.getElementById("boximg6").src == null)
+ if (document.getElementById("boximg6 ").src === null)
   document.getElementById("boximg6").src = "https://image.freepik.com/free-vector/404-error-web-template-with-mad-cat_23-2147763345.jpg"
 }
 
