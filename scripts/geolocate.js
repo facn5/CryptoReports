2739f06afbcae3e682abf6662e5b56a2164fc56f
Karam Ashqar <46317630+karam1ashqar@users.noreply.github.com>
2019-03-13 12:59:21 +0200

    closed parenthesis

--- scripts/geolocate.js
+++ scripts/geolocate.js
@@ -6,7 +6,7 @@ fetch(`https://api.ipdata.co/?api-key=866650af2960a00a8a11b656befb76c50aaf1b2f54
   })
   .then(function(data) {
     console.log(data);
-    console.log(data['continent_name'];
+    console.log(data['continent_name']);
     continent = data['continent_name'];
   })
   .catch(function(error) {
